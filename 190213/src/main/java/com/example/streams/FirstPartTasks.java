da80e4591f833a3219e5f1ab0e5ee34097e1e683
Maria Tigina <tigmah@mail.ru>
2019-02-13 17:53:07 +0300

    added countDupl

--- 190213/src/main/java/com/example/streams/FirstPartTasks.java
+++ 190213/src/main/java/com/example/streams/FirstPartTasks.java
@@ -39,18 +39,18 @@ public final class FirstPartTasks {
 
     // Сгруппировать альбомы по артистам (в качестве значения вместо объекта 'Artist' использовать его имя)
     public static Map<Artist, List<String>> groupByArtistMapName(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+        return null; //albums.collect(Collectors.groupingBy(Album::getArtist, Collectors.mapping(Artist::getName, Collectors.toList())));
     }
 
     // Число повторяющихся альбомов в потоке
     public static long countAlbumDuplicates(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+        return albums.collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().filter( p -> p.getValue() > 1 ).count();
     }
 
     // Альбом, в котором максимум рейтинга минимален
     // (если в альбоме нет ни одного трека, считать, что максимум рейтинга в нем --- 0)
     public static Optional<Album> minMaxRating(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     // Список альбомов, отсортированный по убыванию среднего рейтинга его треков (0, если треков нет)
