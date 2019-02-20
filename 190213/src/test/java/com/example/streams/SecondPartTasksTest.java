14eb31919888bc6730e75c0d2ffe419456ca2560
Zuev Maxim <max-zu99@mail.ru>
2019-02-20 21:41:25 +0300

    remove second part tests

--- 190213/src/test/java/com/example/streams/SecondPartTasksTest.java
+++ 190213/src/test/java/com/example/streams/SecondPartTasksTest.java
@@ -8,21 +8,21 @@ public class SecondPartTasksTest {
 
     @Test
     public void testFindQuotes() {
-        fail();
+
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+
     }
 }
