7e9da44c035d8807c2f82d51c368841fa450530e
Zuev Maxim <max-zu99@mail.ru>
2019-02-20 16:47:54 +0300

    project init

---
+++ 190220/src/main/java/com/hse/java/serialization/Serialization.java
@@ -0,0 +1,15 @@
+package com.hse.java.serialization;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Serialization {
+    public static void serialize(Object object, OutputStream out) {
+
+    }
+
+    public static <T> T deserialize(InputStream in, Class<T> clazz) {
+
+        return null;
+    }
+}
---
+++ 190220/src/test/java/com/hse/java/serialization/SerializationTest.java
@@ -0,0 +1,16 @@
+package com.hse.java.serialization;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SerializationTest {
+
+    @Test
+    void serialize() {
+    }
+
+    @Test
+    void deserialize() {
+    }
+}
