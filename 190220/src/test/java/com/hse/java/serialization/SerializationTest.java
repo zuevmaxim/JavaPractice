a5a94c3c0a1a844cf43b4a3fec0450371bbd1ff8
Maria Tigina <tigmah@mail.ru>
2019-02-20 17:27:29 +0300

    added tests

--- 190220/src/test/java/com/hse/java/serialization/SerializationTest.java
+++ 190220/src/test/java/com/hse/java/serialization/SerializationTest.java
@@ -1,13 +1,53 @@
 package com.hse.java.serialization;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SerializationTest {
 
+    private static class Product {
+        public String name;
+        public boolean hasName;
+        private static byte hasInfo = 1;
+        short weight;
+        final int number = 10;
+        public long id;
+        private float fats;
+        double sugar;
+        char vitamin;
+
+        public Product(String name, boolean hasName, short weight, long id, float fats, double sugar, char vitamin) {
+            this.name = name;
+            this.hasName = hasName;
+            this.weight = weight;
+            this.id = id;
+            this.fats = fats;
+            this.sugar = sugar;
+            this.vitamin = vitamin;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Product) {
+                return  name == ((Product) obj).name &&
+                        hasName == ((Product) obj).hasName &&
+                        hasInfo == ((Product) obj).hasInfo &&
+                        weight == ((Product) obj).weight &&
+                        number == ((Product) obj).number &&
+                        id == ((Product) obj).id &&
+                        fats == ((Product) obj).fats &&
+                        sugar == ((Product) obj).sugar &&
+                        vitamin == ((Product) obj).vitamin;
+            }
+            return false;
+        }
+    }
+
     @Test
     void serialize() {
+
     }
 
     @Test
