0ba031fbd8f048fbf1318c7ab8f8658e696a50a3
Zuev Maxim <max-zu99@mail.ru>
2019-02-27 16:47:36 +0300

    project init

---
+++ 190227a/src/main/java/com/hse/java/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.hse.java.calculator;
+
+import java.util.Stack;
+
+public class Calculator {
+    private final Stack<Integer> stack;
+
+    public Calculator(Stack<Integer> stack) {
+        this.stack = stack;
+    }
+
+    public int evaluate(String s) {
+        return 0;
+    }
+}
---
+++ 190227a/src/test/java/com/hse/java/calculator/CalculatorTest.java
@@ -0,0 +1,10 @@
+package com.hse.java.calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void evaluate() {
+    }
+}
