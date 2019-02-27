ea570e8b3c1aa6e498dbea945ba6087851526bea
Zuev Maxim <max-zu99@mail.ru>
2019-02-27 17:47:36 +0300

    add tests

--- _190227/src/test/java/com/hse/java/calculator/CalculatorTest.java
+++ _190227/src/test/java/com/hse/java/calculator/CalculatorTest.java
@@ -19,6 +19,7 @@ class CalculatorTest {
         Stack<Integer> stack = (Stack<Integer>) mock(Stack.class);
         when(stack.push(anyInt())).thenReturn(null);
         when(stack.pop()).thenThrow(new EmptyStackException());
+        when(stack.peek()).thenThrow(new UnsupportedOperationException());
         calculator = new Calculator(stack);
         assertEquals(0, calculator.evaluate(""));
     }
@@ -26,14 +27,33 @@ class CalculatorTest {
     @Test
     void evaluateSimpleString() {
         Stack<Integer> stack = (Stack<Integer>) mock(Stack.class);
-        when(stack.push(anyInt())).thenReturn(null);
-        when(stack.pop()).thenReturn(1).thenReturn(2).thenReturn(3);
+        when(stack.push(1)).thenReturn(1);
+        when(stack.push(2)).thenReturn(2);
+        when(stack.push(3)).thenReturn(3);
+        when(stack.pop()).thenReturn(1).thenReturn(2);
+        when(stack.peek()).thenReturn(3);
         calculator = new Calculator(stack);
         assertEquals(3, calculator.evaluate("1 2 +"));
     }
 
     @Test
-    void emptyTest() {
+    void evaluate() {
+        Stack<Integer> stack = (Stack<Integer>) mock(Stack.class);
+        when(stack.push(1)).thenReturn(1);
+        when(stack.push(2)).thenReturn(2);
+        when(stack.push(3)).thenReturn(3);
+        when(stack.push(3)).thenReturn(3);
+        when(stack.push(9)).thenReturn(9);
+        when(stack.pop()).thenReturn(1).thenReturn(2).thenReturn(3).thenReturn(3).thenReturn(9);
+        when(stack.peek()).thenReturn(9);
+        calculator = new Calculator(stack);
+        assertEquals(9, calculator.evaluate("1 2 + 3 *"));
+    }
 
+    @Test
+    void stackTest() {
+        Stack<Integer> stack = new Stack<>();
+        calculator = new Calculator(stack);
+        assertEquals(9, calculator.evaluate("1 2 + 3 *"));
     }
 }
