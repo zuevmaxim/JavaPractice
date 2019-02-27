3fcf2675b49f84aba8bd6bc8a6edebf08120adb0
Zuev Maxim <max-zu99@mail.ru>
2019-02-27 17:04:36 +0300

    add first test

--- 190227a/src/test/java/com/hse/java/calculator/CalculatorTest.java
+++ 190227a/src/test/java/com/hse/java/calculator/CalculatorTest.java
@@ -1,10 +1,23 @@
 package com.hse.java.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    private Calculator calculator;
 
-    @org.junit.jupiter.api.Test
-    void evaluate() {
+    @Test
+    void evaluateOnEmptyString() {
+        Stack<Integer> stack = (Stack<Integer>) mock(Stack.class);
+        when(stack.push(anyInt())).thenReturn(null);
+        when(stack.pop()).thenThrow(new EmptyStackException());
+        calculator = new Calculator(stack);
+        assertEquals(0, calculator.evaluate(""));
     }
 }
