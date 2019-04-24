3bb24676aeb6feea05b545d1ab7105931d3fa9de
Lev Levin <combolev@gmail.com>
2019-04-24 17:01:58 +0300

    Add methods in Logic

--- 190424/src/main/java/com/hse/java/xo/Logic.java
+++ 190424/src/main/java/com/hse/java/xo/Logic.java
@@ -1,11 +1,32 @@
 package com.hse.java.xo;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Logic {
-    public Logic(TicTacToe.Viewer viewer) {
+
+    @NotNull private TicTacToe.Viewer viewer;
+    private XO[][] board = {{XO.EMPTY, XO.EMPTY, XO.EMPTY},
+                            {XO.EMPTY, XO.EMPTY, XO.EMPTY},
+                            {XO.EMPTY, XO.EMPTY, XO.EMPTY}};
+    private XO turn = XO.X;
+
+    public Logic(@NotNull TicTacToe.Viewer viewer) {
+        this.viewer = viewer;
+    }
+
+    public XO pushedButton(int x, int y) throws  IllegalArgumentException {
+        if (x > 2 || x < 0 || y > 2 || y < 0) {
+            throw new IllegalArgumentException("Position is out of board!!!");
+        }
+
 
     }
 
-    public XO pushedButton() {
-        return XO.X;
+    private void changeTurn() {
+        if (turn == XO.X) {
+            turn = XO.O;
+        } else {
+            turn = XO.X;
+        }
     }
 }
