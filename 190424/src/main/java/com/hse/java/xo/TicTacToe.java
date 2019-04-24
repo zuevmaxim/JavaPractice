80e06c3294f02642040f275e870a135da52d0771
Zuev Maxim <max-zu99@mail.ru>
2019-04-24 17:29:15 +0300

    add buttons

--- 190424/src/main/java/com/hse/java/xo/TicTacToe.java
+++ 190424/src/main/java/com/hse/java/xo/TicTacToe.java
@@ -2,8 +2,15 @@ package com.hse.java.xo;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.HPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 
@@ -14,11 +21,30 @@ public class TicTacToe extends Application {
         launch(args);
     }
 
+    final int N = 3;
+    Button[][] buttons = new Button[N][N];
+
     @Override
     public void start(Stage primaryStage) {
-        var button = new Button("Click me");
-        button.setOnAction(value -> Platform.exit());
-        var scene = new Scene(button, 200, 100);
+        GridPane root = new GridPane();
+
+        Label labelTitle = new Label("Tic-Tac-Toe Game");
+
+        // Put on cell (0,0), span 2 column, 1 row.
+        root.add(labelTitle, 0, 0, 2, 1);
+
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                buttons[i][j] = new Button(".");
+                buttons[i][j].setOnAction(result -> {
+
+                });
+            }
+        }
+
+
+        Scene scene = new Scene(root, 300, 300);
+        primaryStage.setTitle("GridPanel Layout Demo (o7planning.org)");
         primaryStage.setScene(scene);
         primaryStage.show();
 
