87655b51bad682d06acbd84ec39709043789a70c
Lev Levin <combolev@gmail.com>
2019-04-24 17:28:23 +0300

    Add realization for some methods in Logic

--- 190424/src/main/java/com/hse/java/xo/Logic.java
+++ 190424/src/main/java/com/hse/java/xo/Logic.java
@@ -1,25 +1,36 @@
 package com.hse.java.xo;
 
-import org.jetbrains.annotations.NotNull;
-
 public class Logic {
 
-    @NotNull private TicTacToe.Viewer viewer;
+    private TicTacToe.Viewer viewer;
     private XO[][] board = {{XO.EMPTY, XO.EMPTY, XO.EMPTY},
                             {XO.EMPTY, XO.EMPTY, XO.EMPTY},
                             {XO.EMPTY, XO.EMPTY, XO.EMPTY}};
     private XO turn = XO.X;
+    private int step = 0;
 
-    public Logic(@NotNull TicTacToe.Viewer viewer) {
+    public Logic(TicTacToe.Viewer viewer) {
         this.viewer = viewer;
     }
 
-    public XO pushedButton(int x, int y) throws  IllegalArgumentException {
+    public void pushedButton(int x, int y) {
         if (x > 2 || x < 0 || y > 2 || y < 0) {
-            throw new IllegalArgumentException("Position is out of board!!!");
+            return;
+        } else if (board[x][y] != XO.EMPTY) {
+            return;
         }
 
+        viewer.set(x, y, turn);
+        board[x][y] = turn;
+        step++;
 
+        if (isVictory(x, y)) {
+            viewer.showResult(turn);
+        } else if (step == 9) {
+            viewer.showResult(XO.EMPTY);
+        } else {
+
+        }
     }
 
     private void changeTurn() {
@@ -28,5 +39,29 @@ public class Logic {
         } else {
             turn = XO.X;
         }
+    }
+
+    private boolean isVictory(int x, int y) {
+        if (board[x][0] == turn && board[x][1] == turn && board[x][2] == turn) {
+            return true;
+        }
+
+        if (board[0][y] == turn && board[1][y] == turn && board[2][y] == turn) {
+            return true;
+        }
+
+        if (x == y) {
+            if (board[0][0] == turn && board[1][1] == turn && board[2][2] == turn) {
+                return true;
+            }
+        }
+
+        if (x + y == 2) {
+            if (board[2][0] == turn && board[1][1] == turn && board[0][2] == turn) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
